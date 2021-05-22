d626e16317dc9a537d0eb617d98fad61b75dd6fc
FedorLivshits <fediliv@gmail.com>
2021-05-22 14:49:56 +0300

    added localStorage

--- script/main.js
+++ script/main.js
@@ -2,21 +2,33 @@ let input = document.querySelector('input');
 let addBtn = document.querySelector('.add-btn');
 let todoList = document.querySelector('.todoList');
 let clearAllBtn = document.querySelector('.clear-all__btn');
+let checkboxBtn = document.querySelector('.checkbox');
 
 let tasks = []
+if(localStorage.getItem('todoList')){
+    tasks = JSON.parse(localStorage.getItem('todoList'))
+    showTasks();
+}
+let num = 1
 addBtn.addEventListener('click', () => {
     let task = {
+        id: num++,
         text: input.value,
         checked: false
     }
     tasks.push(task);
     input.value = "";
     showTasks();
+    localStorage.setItem('todoList', JSON.stringify(tasks))
 })
-const showTasks = () => {
+function showTasks() {
     let newTodoElement = '';
     tasks.map((item, index) => {
-        newTodoElement += `<li id=${index}> <input type="checkbox" id=${index}> ${item.text}<span class="icon"><i class="fas fa-trash"></i></span></li>`
+        newTodoElement += `<li id=${index}> <input class="checkbox" type="checkbox" ${item.checked ? 'checked' : ''}> ${item.text} <span class="icon"><i class="fas fa-trash"></i></span></li>`
         todoList.innerHTML = newTodoElement
     })
 }
+
+// checkboxBtn.addEventListener("click", () =>{
+//     tasks.filter(item => item.id === )
+// })
