fb774f86818dd874c0b2c0bfcb48f78e7f82faec
FedorLivshits <fediliv@gmail.com>
2021-05-22 22:08:42 +0300

    added tasks filter

--- script/main.js
+++ script/main.js
@@ -2,29 +2,30 @@ let input = document.querySelector('input');
 let addBtn = document.querySelector('.add-btn');
 let todoList = document.querySelector('.todoList');
 let clearAllBtn = document.querySelector('.clear-all__btn');
+let pendingTasksNumb = document.querySelector(".pendingTasks");
 
-let tasks = []
+let tasks = [];
 
 if (localStorage.getItem('todoList')) {
-    tasks = JSON.parse(localStorage.getItem('todoList'))
+    tasks = JSON.parse(localStorage.getItem('todoList'));
     showTasks();
 }
 if (input.value === "") {
-    addBtn.classList.add('disabled')
+    addBtn.classList.add('disabled');
 }
 
 input.onkeyup = () => {
     if (input.value !== "") {
-        addBtn.classList.remove('disabled')
+        addBtn.classList.remove('disabled');
     } else {
-        addBtn.classList.add('disabled')
+        addBtn.classList.add('disabled');
     }
 }
 addBtn.addEventListener('click', () => {
-    let inputValue = input.value
+    let inputValue = input.value;
     if (inputValue) {
         if (inputValue.length > 25) {
-            inputValue = inputValue.substr(0, 25) + '...'
+            inputValue = inputValue.substr(0, 25) + '...';
         }
         let task = {
             id: null,
@@ -34,28 +35,31 @@ addBtn.addEventListener('click', () => {
         tasks.push(task);
         input.value = "";
         showTasks();
-        localStorage.setItem('todoList', JSON.stringify(tasks))
+        localStorage.setItem('todoList', JSON.stringify(tasks));
     }
 })
 
-function showTasks() {
+function showTasks(arr = tasks) {
+    let array = arr;
     if (!tasks.length) {
         todoList.innerHTML = "";
-        clearAllBtn.classList.add('disabled')
-    }else{
-        clearAllBtn.classList.remove('disabled')
+        clearAllBtn.classList.add('disabled');
+    } else {
+        clearAllBtn.classList.remove('disabled');
     }
     let newTodoElement = '';
-    tasks.map((item, index) => {
-        newTodoElement +=
-            `<li class="list">
+    const taskListMapping = () => {
+        array.map((item, index) => {
+            newTodoElement +=
+                `<li class="list">
                             <input class="checkbox" type="checkbox" id=${item.id = index} ${item.checked ? 'checked' : ''}>
                              ${item.text} 
                             <span id=${index} onclick="deleteTask(${index})" class="icon delete-btn"><i class="fas fa-trash"></i></span>
                 </li>`
-        todoList.innerHTML = newTodoElement
-    })
-    let pendingTasksNumb = document.querySelector(".pendingTasks");
+            todoList.innerHTML = newTodoElement;
+        })
+    }
+    taskListMapping();
     pendingTasksNumb.textContent = (tasks.length).toString();
 }
 
@@ -63,8 +67,8 @@ todoList.addEventListener("change", (e) => {
     let currentId = e.target.getAttribute('id');
     tasks.filter(item => {
         if (item.id === +currentId) {
-            item.checked = !item.checked
-            localStorage.setItem('todoList', JSON.stringify(tasks))
+            item.checked = !item.checked;
+            localStorage.setItem('todoList', JSON.stringify(tasks));
         }
     })
 })
@@ -82,3 +86,29 @@ clearAllBtn.addEventListener('click', () => {
     localStorage.setItem('todoList', JSON.stringify(tasks));
     showTasks();
 })
+
+function taskFilter() {
+    let allTasks = document.querySelector('.all-tasks');
+    let progressTasks = document.querySelector('.in-progress__tasks');
+    let doneTasks = document.querySelector('.done-tasks');
+
+    const changeFilter = (arr) => {
+        localStorage.setItem('todoList', JSON.stringify(arr));
+        showTasks(arr);
+        pendingTasksNumb.textContent = (arr.length).toString();
+    }
+    allTasks.onclick = () => {
+        let newAll = tasks.filter(item => item);
+        changeFilter(newAll);
+    }
+    progressTasks.onclick = () => {
+        let newProgress = tasks.filter(item => !item.checked);
+        changeFilter(newProgress);
+    }
+    doneTasks.onclick = () => {
+        let newDone = tasks.filter(item => item.checked);
+        changeFilter(newDone);
+    }
+}
+
+taskFilter();
